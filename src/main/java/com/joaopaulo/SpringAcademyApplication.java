9eaa3dd39e5ec8de39b88f7eeb52fdb2cb607070
Dundaki <vigjoaopaulo@gmail.com>
2022-11-22 18:58:30 -0300

    inicio do projeto

---
+++ src/main/java/com/joaopaulo/SpringAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.joaopaulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAcademyApplication.class, args);
+	}
+
+}
